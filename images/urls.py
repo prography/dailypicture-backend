c139265f498c4777ade59b9ac049bc71cd041643
soomin <soomtopia@gmail.com>
2019-11-08 00:18:06 +0900

    1. add poor code - image url / view

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,14 @@ from . import views
 app_name = 'images'
 
 urlpatterns = [
-    
+    #  image create
+    path('create', views.create, name='create'),
+    # #  get > post images list
+    path('<int:post_id>/list', views.list, name='lists'),
+    #  get > image detail
+    path('<int:id>', views.detail),
+    #  delete > image 
+    path('<int:id>', views.detail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- images/views.py
+++ images/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from posts.models import Post
+from django.views.decorators.csrf import csrf_exempt
+from .models import Image
 
-# Create your views here.
+
+@csrf_exempt
+def create(request):
+    # post 도 get 으로 바꿔야함
+    if request.method == "POST":
+        # post_id = 1 # dumy. 바꿔야함
+        post = Post.objects.get(pk=1)
+        image = Image.objects.create(post_id=post, url="https://github.com/jangjichang/Today-I-Learn/raw/master/Algorithm/theory/stack.jpg?raw=true")
+        return render(request, 'images/create.json', {'image': request.POST})
+
+def detail(request, id):
+    image = Image.objects.get(pk=id)
+    if request.method == "GET":
+        return render(request, 'images/detail.html', {'image':image})
+    
+    if request.method == "DELETE":
+        return render(request, 'images/delete.html', {'image':"image delete complete"})
+
+
+# 목록에 해당하는 사진 리스트만 반환한다
+def list(request,post_id):
+    if request.method == "GET":
+        post = Post.objects.get(pk=post_id)
+        images = Image.objects.filter(post_id=post)
+        if bool(images):
+            return render(request,'images/list.html',{'images' : images})
