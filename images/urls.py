2e4d752b5f0e0e65886ad5c3c0ce7fb8e09302ec
soomin <soomtopia@gmail.com>
2019-11-08 23:05:47 +0900

    4. refactoring drf tutorial 3. classed based view

--- images/urls.py
+++ images/urls.py
@@ -7,9 +7,9 @@ app_name = 'images'
 
 urlpatterns = [
     # get => image list, post => create
-    path('<int:post_id>/list', views.list),
+    path('<int:post_id>/list', views.Imagelist.as_view()),
     # get => detail, delete => delete image
-    path('<int:id>', views.detail),
+    path('<int:id>', views.ImageDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- images/views.py
+++ images/views.py
@@ -1,43 +1,49 @@
 # about image application import
 # about django rest framework import 
-# 1 -> 2 
-# remove json parser > response 
-# remove csrf exempt > api_view
+# 2 -> 3 
+# add HTTP404
+# change apiview module import > decorate > viees
 from .models import Image
 from posts.models import Post
 from .serializers import ImageSerializer
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
+class Imagelist(APIView):
+    def valid_post_object(self,pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
 
-@api_view(['GET', 'POST'])
-def list(request, post_id, format=None):
-    if request.method == "GET":
-        post = Post.objects.get(pk=post_id)
+    def get(self, request, post_id, format=None):
+        post = self.valid_post_object(post_id)
         images = Image.objects.filter(post_id=post)
         serializer = ImageSerializer(images, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request, post_id, format=None):
         serializer = ImageSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'DELETE'])     
-def detail(request, id, format=None):
-    try:
-        image = Image.objects.get(pk=id)
-    except Image.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class ImageDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Image.objects.get(pk=id)
+        except Image.DoesNotExist:
+            raise Http404
 
-    if request.method == "GET":
-        serializer = ImageSerializer(image)
+    def get(self, request, id, format=None):
+        serializer = ImageSerializer(self.get_object(id))
         return Response(serializer.data)
 
-    elif request.method == "DELETE":
+    def delete(self, request, id, format=None):
+        image = self.get_object(id)
         image.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
