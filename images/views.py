e0fba842ec8cb8843346343ee7977355b96a7adc
soomin <soomtopia@gmail.com>
2019-11-09 00:02:41 +0900

    4. refactoring drf tutorial 3. classed based view - using generics

--- images/views.py
+++ images/views.py
@@ -10,21 +10,10 @@ from .serializers import ImageSerializer
 from rest_framework import mixins
 from rest_framework import generics
 
-class Imagelist(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class Imagelist(generics.ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     
-    def get(self, request, *args, **kwargs):
-            return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class ImageDetail(mixins.RetrieveModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class ImageDetail(generics.RetrieveDestroyAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
