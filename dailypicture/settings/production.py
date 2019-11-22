469883d3c3644c01c68918bef2e6d4fba01aca72
Sungho Yoon <lacrimak3@gmail.com>
2019-11-22 18:03:33 +0900

    [1203] 미디어 파일 S3 Bucket 연결
    S3 bucket 연결을 위한 의존성 추가
    production 설정일 때 filestorage를 s3로 변경

--- dailypicture/settings/production.py
+++ dailypicture/settings/production.py
@@ -18,3 +18,16 @@ DATABASES = {
         'PORT': get_secret("DJANGO_DB_PORT"),
     }
 }
+
+
+# S3 storage
+DEFAULT_FILE_STORAGE = 'dailypicture.settings.storages.MediaStorage'
+#STATICFILES_STORAGE = 'dailypicture.settings.storages.StaticStorage'
+MEDIAFILES_LOCATION = 'media'
+#STATICFILES_LOCATION = 'static'
+
+# AWS Access
+AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
