85e60746aab876cf6b6b4696215aec6845c86437
Sungho Yoon <lacrimak3@gmail.com>
2019-11-25 23:17:29 +0900

    [1303] 저장소가 s3/로컬일때 분리
    저장소가 S3일때 작동하는 함수는 ~S3
    로컬일때는 ~Local로 각각 분리함

--- videos/timelapse.py
+++ videos/timelapse.py
@@ -7,46 +7,66 @@ import boto3
 
 
 class Timelapse:
-    temp_image_path = "./temp_image"
+    image_base_path = "./temp_image"
     video_base_path = "./temp_video"
     fps = 60
     imageps = 5
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    )
+    try:
+        s3 = True
+        s3client = boto3.client(
+            "s3",
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        )
+    except AttributeError:
+        s3 = False
 
     def __init__(self, image_urls, title, post_id):
         self.image_urls = image_urls
         self.title = title
-        self.image_path = os.path.join(self.temp_image_path, str(post_id))
+        self.image_path = os.path.join(self.image_base_path, post_id)
         self.post_id = post_id
-        self.video_path = os.path.join(self.video_base_path, str(post_id))
+        self.video_path = os.path.join(self.video_base_path, post_id)
 
         if not os.path.isdir(self.image_path):
             os.makedirs(self.image_path)
         if not os.path.isdir(self.video_path):
             os.makedirs(self.video_path)
 
-        self.save()
+        if self.s3:
+            self.saveFromS3()
+        else:
+            upload_path = os.path.join(settings.MEDIA_ROOT, "video", post_id)
+            if not os.path.isdir(upload_path):
+                os.makedirs(upload_path)
+            self.saveFromLocal()
 
     # 저장소에서 서버로 이미지들을 다운로드
-    def save(self):
+    def saveFromS3(self):
         self.image_list = []
-        for url in self.image_urls:
-            image_name = os.path.join(self.image_path, url.split("/")[-1])
-            self.image_list.append(image_name)
-            self.s3.download_file(
-                settings.AWS_STORAGE_BUCKET_NAME, "media/" + url, image_name
+        for image in self.image_urls:
+            image_base_path = os.path.join(self.image_path, image.name.split("/")[-1])
+            self.image_list.append(image_base_path)
+            self.s3client.download_file(
+                settings.AWS_STORAGE_BUCKET_NAME, "media/" + image.name, image_base_path
             )
+
+    def saveFromLocal(self):
+        self.image_list = []
+        for image in self.image_urls:
+            image_base_path = os.path.join(self.image_path, image.name.split("/")[-1])
+            self.image_list.append(image_base_path)
+            image_path = image.path
+            shutil.copy2(image_path, self.image_path)
 
     # 변환 후 임시로 저장한 이미지 삭제
     def delete(self):
         if os.path.isdir(self.image_path):
             shutil.rmtree(self.image_path)
+        if os.path.isdir(self.video_path):
+            shutil.rmtree(self.video_path)
 
-    # 비디오 변환
+    # 영상 변환
     def make(self):
         images = [cv2.imread(image) for image in self.image_list]
         height, width, channel = images[0].shape
@@ -60,15 +80,28 @@ class Timelapse:
                 writer.write(frame)
         writer.release()
 
-        upload_file_name = "video/" + str(self.post_id) + "/" + self.title + ".mp4"
+        upload_file_name = "video/" + self.post_id + "/" + self.title + ".mp4"
+
+        if self.s3:
+            self.saveVideoToS3(upload_file_name, video)
+        else:
+            self.saveVideoToLocal(upload_file_name, video)
+
+        self.video_url = default_storage.url(name=upload_file_name)
+        self.delete()
+        return self.video_url
+
+    # MEDIA_ROOT에 영상 저장
+    def saveVideoToLocal(self, upload_file_name, video):
+        with open(video, "rb") as video_byte:
+            with default_storage.open(upload_file_name, "wb") as save_file:
+                save_file.write(video_byte.read())
+
+    def saveVideoToS3(self, upload_file_name, video):
         upload_file_path = "media/" + upload_file_name
-        self.s3.upload_file(
+        self.s3client.upload_file(
             video,
             settings.AWS_STORAGE_BUCKET_NAME,
             upload_file_path,
             ExtraArgs={"ACL": "public-read"},
         )
-
-        self.video_url = default_storage.url(name=upload_file_name)
-        self.delete()
-        return self.video_url
