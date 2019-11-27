704b6b4ce393932d65dcb88e92ee7e1ace3a37f6
soomin <soomtopia@gmail.com>
2019-11-27 21:35:15 +0900

    image url changed

--- images/models.py
+++ images/models.py
@@ -4,7 +4,7 @@ from posts.models import Post
 class Image(models.Model):
     post_id = models.ForeignKey(Post, verbose_name='게시글', on_delete=models.CASCADE, related_name='images')
     created_at = models.DateTimeField('작성시간',auto_now_add=True)
-    url = models.ImageField('사진', upload_to="post/image/{%post_id%}/%Y%m%d")
+    url = models.ImageField('사진', upload_to="posts/images/")
 
     class Meta:
         ordering = ['created_at']
