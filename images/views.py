a7050aaaad8b801cb0de5a8d0764763f95c91c0e
soomin <soomtopia@gmail.com>
2019-12-25 02:09:58 +0900

    [2202] convert pep8 maybe..

--- images/views.py
+++ images/views.py
@@ -1,26 +1,21 @@
-# about image application import
-# about django rest framework import 
-# 2 -> 3 
-# add HTTP404
-# change apiview module import > decorate > viees
-# remove and add mixins / generics
 from .models import Image
-from posts.models import Post
 from .serializers import ImageSerializer
-from rest_framework import mixins
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
+from posts.models import Post
 
-# from rest_framework import Response
-class Imagelist(generics.CreateAPIView):
-    queryset = Image.objects.all()
+
+class ImageCreate(generics.CreateAPIView):
     serializer_class = ImageSerializer
+
     def perform_create(self, serializer):
-        serializer.save(post_id = self.request.data['post_id'])
-        return super().perform_create(serializer)
-    
+        post_id = self.request.data['post']
+        post = Post.objects.get(pk=post_id)
+        super().perform_create(serializer)
+        serializer.save(post=post)
+        return 
+
 
 class ImageDetail(generics.RetrieveDestroyAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
