dfcc3f8a3de9dc85978e84d44110bc1d430f9213
soomin <soomtopia@gmail.com>
2019-12-25 02:08:26 +0900

    add days_count data

--- images/serializers.py
+++ images/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import *
+from posts.models import Post
 
-class ImageSerializer(serializers.HyperlinkedModelSerializer):
+
+class ImageSerializer(serializers.ModelSerializer):
+    days_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
-        fields = ['id', 'url']
+        fields = ['id', 'url', 'days_count']
+
+    def get_days_count(self, obj):
+        start_day = Post.objects.get(images=obj.id).created_at
+        return (obj.created_at - start_day).days
