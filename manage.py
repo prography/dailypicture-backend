5b2f134aa629a6fb4279d9304989b3414de6992f
soomin <soomtopia@gmail.com>
2020-01-17 18:12:03 +0900

    setting mode change

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailypicture.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailypicture.settings.development')
 
     try:
         from django.core.management import execute_from_command_line
