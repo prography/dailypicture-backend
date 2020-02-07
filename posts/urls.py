2e47629d9d10afac23513939e57aee88fd3a4f28
jisoo <cjisoo0117@gmail.com>
2020-02-07 17:24:29 +0900

    [3201] post viewset으로 리팩토링

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 from rest_framework.routers import DefaultRouter
-from rest_framework_swagger import renderers
 
 app_name = 'posts'
-# router = DefaultRouter()
-# router.register(r'', PostViewSet)
+router = DefaultRouter()
+router.register(r'', PostViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
-    path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
+    path('', include(router.urls)),
+
+    # path('', PostList.as_view()),
+    # path('<int:pk>/', PostDetail.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,74 +1,26 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import *
 from .serializers import *
-from rest_framework import permissions, generics
-from rest_framework.response import Response
-from rest_framework import status
 from dailypicture.permissions import IsOwner
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
+from .models import *
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwner]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(owner=self.request.user)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return PostDetailSerializer
+        return PostSerializer
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def list(self, request):
-        queryset = Post.objects.filter(owner=request.user)
-        serializer = PostSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
-    queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
-
-
-
-
-# class PostViewSet(viewsets.ModelViewSet):
-#     '''
-#         목표
-#         ---
-#         /api-token-auth 에서 사용자 인증 토큰을 발급 받은 후,
-#         header에 `Authorization: Token 토큰 값` 형식으로 토큰을 넣어줘야 함
-
-        
-#         ```
-#         원래는 로그인한 사용자만 접근 가능한데 url 보라고 열어두겠음
-#         인증 안된 사용자가 요청하면
-#         status : 401
-#         {
-#             "detail": "자격 인증데이터(authentication credentials)가 제공되지 않았습니다."
-#         }
-#         이 return 됨
-#         ```
-#     '''
-
-#     # permission_classes = [IsAuthenticated, IsOwner]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-#         print(self.request.user)
-    
-#     def create(self, request):
-#         if request.user.is_anonymous :
-#             # content = {'detail': self.request.user.username}
-#             content = {'request' : self.request.user.username}
-#             return Response(content, status=status.HTTP_404_NOT_FOUND) 
-
-#     def list(self, request):
-#         if request.user.is_anonymous :
-#             # content = {'detail': '로그인을 해주세요'}
-#             content = {'request' : self.request.user.username}
-
-#             return Response(content, status=status.HTTP_404_NOT_FOUND)
-#         else:
-#             queryset = Post.objects.filter(user=request.user)
-#             serializer = PostSerializer(queryset, many=True)
-#             return Response(serializer.data)
+    def perform_destroy(self, instance):
+        for image in instance.images.all():
+            image.url.delete(save=False)
+        instance.delete()
