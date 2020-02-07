d72188d6f1959573bea640af053a42035159db4a
jisoo <cjisoo0117@gmail.com>
2020-02-07 18:29:55 +0900

    [3201] post thumbnail 업로드 이름 변경

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from accounts.models import User
+import datetime
+
+def image_directory_path(instance, filename):
+    return f'account/thumbnail/{datetime.datetime.now().strftime("%y%m%d%H:%M:%S")}.jpg'
 
 class Post(models.Model):
     owner = models.ForeignKey(User, verbose_name='작성자', on_delete=models.CASCADE, related_name='my_post')
     created_at = models.DateField('작성시간',auto_now_add=True)
     title = models.CharField('제목',max_length=300)
-    thumbnail = models.ImageField('썸네일', upload_to="account/%Y%m%d%H:%M:%S")
+    thumbnail = models.ImageField('썸네일', upload_to=image_directory_path)
     status = models.BooleanField('상태', default=False)
 
     class Meta:
